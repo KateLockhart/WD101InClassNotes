0423eb6a24229faf1bdb00bca92b47e7b7c8bbbd
KateLockhart <katelockhart707@gmail.com>
2021-09-01 14:41:11 -0400

    First commit of WD101PT in class lecture notes covering Week 1 to the start of Week 3.

---
+++ functions.js
@@ -0,0 +1,30 @@
+/*
+Types of Functions:
+- function declaration
+- function expression
+- anonymous function
+*/
+
+declaration();
+
+// Function declaration: are hoisted, run when we call them(invoke)
+function declaration() {
+    console.log("I'm a Function Declaration.");
+}
+
+//expression(); <--errors out 
+
+//Function expression: are NOT hoisted and are stored in a variable
+// We use the variable name to run the function
+let expression = function() { // <-- an example of an anonymous function
+    console.log("I'm a Function Expression!");
+}
+
+expression();
+
+// Function Declaration as well
+function myName(name) {
+    console.log(`My name is ${name}`);
+}
+
+myName("Kate");
